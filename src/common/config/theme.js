2e847727c4d4e8defd9fffb277bd8f59ffe2e51a
HankinsPan <hankins.home@outlook.com>
2020-02-04 15:06:24 +0800

    add fast-image lib

--- src/common/config/theme.js
+++ src/common/config/theme.js
@@ -2,4 +2,18 @@
  * @Description:
  * @author HuiWen
  * @date 2020/2/1
-*/
+ */
+
+export const theme = {
+  color: {
+    main: '#722ed1',
+    sub: '#fff',
+    title: '#292929',
+    subTitle: '#969696',
+    body: '#f7f7f7',
+    divider: '#eee',
+  },
+  fontWeight: {
+    bold: '400'
+  }
+}
